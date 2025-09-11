ccabc4be4e1ad4d377e408bf1befb22e6ca61e05
Eduardo <eduardo_d_maia@estudante.sesisenai.org.br>
2025-09-11 19:42:28 -0300

    Att11 finalizada

---
+++ att11.js
@@ -0,0 +1,21 @@
+const { parse } = require('path');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Digite um numero:',function(numero1){
+    rl.question('Digite outro numero:',function(numero2){
+        rl.question('Digite mais um numero:',function(numero3){
+            let n1=parseInt(numero1)
+            let n2=parseInt(numero2)
+            let n3=parseInt(numero3)
+            let numeros=[n1,n2,n3]
+            numeros.sort((a,b)=>a-b)
+            console.log(numeros)
+
+          rl.close()
+    })    
+    })     
+    })
