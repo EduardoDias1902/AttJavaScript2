90e74f1ef82370aa574508a10ed2d13c5e365f85
eduardo_d_maia <eduardo_d_maia@estudante.sesisenai.org.br>
2025-09-18 16:32:13 -0300

    Att17 finalizada

---
+++ att17.js
@@ -0,0 +1,15 @@
+nomes = ["Akiane B.","Eduardo D.","Vitor A.","Pedro O.","Gustavo S.","André L.","Matheus A."]
+console.log(nomes)
+nomes.push("Gustavo P.");
+console.log(nomes)
+let ultimoNome = nomes.pop();
+console.log(nomes)
+nomes.unshift("Gabrielli V.");
+console.log(nomes)
+let primeiroNome = nomes.shift();
+console.log(nomes)
+
+
+
+
+
